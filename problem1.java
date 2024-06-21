8737f10212be825a3f50cc10dac355ffb292b9a3
Rekha Maruti Garadimani <119103939+Rekhamaruti@users.noreply.github.com>
2024-06-21 10:40:06 +0530

    Update problem1.java

--- problem1.java
+++ problem1.java
@@ -3,3 +3,12 @@ class Solution{
   public int[] twoSum(int[] nums,int target){
     for(int i=0;i<nums.length;i++){
       for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+          return new int[]{};
+        }
+      }
+    }
+  return new int[]{};
+  }
+}
+
