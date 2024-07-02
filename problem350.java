ec7c30cfdee24210703ad9caa2fa525d0d9b6173
Rekha Maruti Garadimani <119103939+Rekhamaruti@users.noreply.github.com>
2024-07-02 11:30:18 +0530

    Create problem350.java

---
+++ problem350.java
@@ -0,0 +1,10 @@
+# Intersection of two arrays
+class Solution{
+  public int[] intersect(int[] nums1,int[] nums2){
+    int l1=nums1.length;
+    int l2=nums2.length;
+    int i=0,j=0,k=0;
+    Array.sort(nums1);
+    Array.sort(nums2);
+  }
+}
